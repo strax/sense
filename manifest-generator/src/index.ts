80be9c1460f643e8c1f4c388adbaf049a0b89ac0
Sami Kukkonen <sami@samikukkonen.fi>
2018-04-08 13:36:12 +0300

    Remove manifest-generator

--- manifest-generator/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as path from "path";
-import * as fs from "fs";
-
-import Project from "./Project";
-import { makeProjectManifest } from "./manifest";
-
-const project = new Project(path.resolve("components"));
-const manifest = makeProjectManifest(project);
-fs.writeFileSync("manifest.json", JSON.stringify(manifest));
